465f0d3c2929505a5a0867edef066fddc44212a8
Dsalas8 <ssdamian1218@gmail.com>
2025-12-15 21:14:59 +0000

    edits

--- day_1_activities/student_data2.py
+++ day_1_activities/student_data2.py
@@ -1,10 +1,19 @@
+Fname = input("what is your first name")
+ID = input("What is your 8 digit ID?")
+LNAME = input("what is your Last Name?")
+MNAME = input("What is your middle name?")
+HR = input("what is your homeroom?")
+GL = input("What is your grade level?")
+EMAIL1 = input("What is your first email?")
+EMAIL2 = input("What is your second email?")
 students = [{
-    "CPSID": ,
-    "Combo,Name": "",
-    "LName": "",
-    "FName": "",
-    "MName": "",
-    "HR": "",
-    "GL": ,
-    "Email": ["karenanderson@example.com", "karen.a@example.net"]
+    "CPSID": ID,
+    "LName": LNAME,
+    "FName": Fname,
+    "MName": MNAME,
+    "HR": HR,
+    "GL": GL,
+    "Email":[EMAIL1, EMAIL2]
 }]
+
+
