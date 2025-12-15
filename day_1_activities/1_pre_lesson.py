91d548b5207ba6b391b2a07ff5923b7016685b7c
Dsalas8 <ssdamian1218@gmail.com>
2025-12-15 20:07:16 +0000

    edits

--- day_1_activities/1_pre_lesson.py
+++ day_1_activities/1_pre_lesson.py
@@ -1,1 +1,32 @@
 # this is what we will use for the video intro to dictionaries
+#dicitonary = a collection of {key:value} pairs
+            #ordered and changable. No duplicates 
+capitals = {"USA": "Washington D.C.",               #Colons give the country the value of the capital, exactly like that.
+            "India":"New Delhi",
+            "China": "Beijing",
+            "Russia": "Moscow"}
+#print(dir(capitals))
+#print(help(capitals))
+print(capitals.get("USA"))
+if capitals.get("Japan"):
+    print("That capital exsists")
+else:
+    print("That capital doesnt exsist")
+capitals.update({"Germany": "Berlin"})
+#capitals.update({"USA": "Detroit"})
+#capitals.pop("China")
+#capitals.popitem()
+#capitals.clear()
+keys = capitals.keys()
+print(capitals)
+for key in capitals.keys():
+    print(key)
+
+values = capitals.values()
+for value in capitals.values():
+    print(value)
+
+items = capitals.items()
+print(items)
+for key, value in capitals.items():
+    print(f"{key}: {value}")
